5e3ee9c35b01fdccb0d014dd2bccc61fd1f0ce47
genesisvb <123412521+genesisvb@users.noreply.github.com>
2023-04-11 22:58:33 -0700

    Add file uploads

--- routes/pets.js
+++ routes/pets.js
@@ -2,15 +2,20 @@ const express = require("express");
 const petsRouter = express.Router();
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 const multer = require("multer");
 const dataFile = "./data/pets.json";
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "/public/images/");
+    callback(null, "./public/images");
+  },
+  filename: function (req, file, callback) {
+    callback(null, `${uuid()}${path.extname(file.originalname)}`);
   },
 });
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
 function writePetsData(data) {
   const newData = JSON.stringify(data);
@@ -27,12 +32,16 @@ petsRouter.get("/", (request, response) => {
   response.status(200).json(readPetsData());
 });
 
+petsRouter.post("/photos", upload.single("photo"), (request, response) => {
+  response.status(200).json({ filename: request.file.filename });
+});
+
 petsRouter.post("/", (request, response) => {
   const petsData = readPetsData();
   const body = request.body;
   const newPet = {
     id: uuid(),
-    photo: "photo url",
+    photo: body.photo,
     species: body.species,
     name: body.name,
     breed: body.breed,
